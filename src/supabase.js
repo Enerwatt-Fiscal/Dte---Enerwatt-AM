ad7cbb85fd240b7fd2b7d0547ca456ed8d2c328a
Enerwatt-Fiscal <karolyne.tome@enerwatt.com.br>
2026-03-11 15:23:09 -0300

    corrige chave supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = "https://uozeeclvruonpdaznhod.supabase.co"
-const SUPABASE_KEY = "sb_publishable_8WEP0OkAST9NxBRx4Gf8Ng_OsTd8I2C"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvemVlY2x2cnVvbnBkYXpuaG9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MjgwNDMsImV4cCI6MjA4ODQwNDA0M30.fDNk57tSvVDB7vY--LE4SaQGLAZtCZ0l89-we9vuv8Y"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
